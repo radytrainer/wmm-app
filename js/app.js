27109c5598edb7a15161eb63c5f59b2496cdc9b4
Rady.Y <Rady.Y@PNC.LAN>
2020-01-31 14:31:08 +0700

    wmm application project initialize

---
+++ js/app.js
@@ -0,0 +1,77 @@
+/**
+ * @function: get all data from guest table
+ * 
+ */
+database.collection('tbl_guest').get().then(
+    (guests) => {
+        const table = document.querySelector('#guest-list');
+        guests.docs.forEach(guest => {
+            const item = `
+                <tr>
+                    <td>
+                        <a href="#"><i class="material-icons red-text">delete</i></a>
+                        <a href="#"><i class="material-icons teal-text">edit</i></a>
+                        <a href="#"><i class="material-icons blue-text">visibility</i></a>
+                    </td>
+                    <td>${guest.data().name}</td>
+                    <td>${guest.data().village}</td>
+                    <td>${guest.data().province}</td>
+                    <td>${guest.data().money}</td>
+                </tr>
+            `;
+            table.insertAdjacentHTML('beforeend', item);
+        });
+    }
+);
+
+/**
+ * @function: get all data from drin table
+ * 
+ */
+database.collection('tbl_drink').get().then(
+    (guests) => {
+        const table = document.querySelector('#drink-list');
+        guests.docs.forEach(guest => {
+            const item = `
+                <tr>
+                    <td>
+                        <a href="#"><i class="material-icons red-text">delete</i></a>
+                        <a href="#"><i class="material-icons teal-text">edit</i></a>
+                        <a href="#"><i class="material-icons blue-text">visibility</i></a>
+                    </td>
+                    <td>${guest.data().drink}</td>
+                    <td>${guest.data().type}</td>
+                    <td>${guest.data().quality}</td>
+                    <td>${guest.data().payment}</td>
+                </tr>
+            `;
+            table.insertAdjacentHTML('beforeend', item);
+        });
+    }
+);
+
+/**
+ * @function: get all food information
+ * 
+ */
+database.collection('tbl_food').get().then(
+    (guests) => {
+        const table = document.querySelector('#food-list');
+        guests.docs.forEach(guest => {
+            const item = `
+                <tr>
+                    <td>
+                        <a href="#"><i class="material-icons red-text">delete</i></a>
+                        <a href="#"><i class="material-icons teal-text">edit</i></a>
+                        <a href="#"><i class="material-icons blue-text">visibility</i></a>
+                    </td>
+                    <td>${guest.data().name}</td>
+                    <td>${guest.data().type}</td>
+                    <td>${guest.data().price}</td>
+                    <td>${guest.data().cooker_name}</td>
+                </tr>
+            `;
+            table.insertAdjacentHTML('beforeend', item);
+        });
+    }
+);
---
+++ js/main.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDZVoOI7vQDVuz9DzoRuC9dZi-HN4hi9MQ",
+  authDomain: "wmm-apps.firebaseapp.com",
+  databaseURL: "https://wmm-apps.firebaseio.com",
+  projectId: "wmm-apps",
+  storageBucket: "wmm-apps.appspot.com",
+  messagingSenderId: "73060976581",
+  appId: "1:73060976581:web:cf89ea536e295fc3e3adac"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const database = firebase.firestore();
+
